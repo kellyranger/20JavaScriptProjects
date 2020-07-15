c77250d0abccaec12a1f50e6d141e3dcca6c5c2c
Kelly Ranger <kelly@losgatos-massage.com>
2020-07-15 12:51:14 -0700

    Hangman error - Cannot set innerHTML of null

---
+++ hangman-kelly/script.js
@@ -0,0 +1,82 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+
+//Show hidden word
+function displayWord(){
+  wordEl.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(letter => `
+      <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+      </span>
+      `).join('')}
+    `;
+  
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if(innerWord === selectedWord){
+    finalMessage.innerText = 'Congratulations! You Won!';
+    popup.style.display = 'flex';
+  }
+
+}
+
+//Update the Wrong Letters array
+function updateWrongLettersEl() {
+  // console.log('Update wrong');
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+}
+
+//Show notification
+function showNotification(){
+  notification.classList.add('show');
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if(e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if(selectedWord.includes(letter)){
+      if(!correctLetters.includes(letter)){
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if(!wrongLetters.includes(letter)){
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      }else {
+        showNotification();
+      }
+    }
+  }
+});
+
+displayWord();
