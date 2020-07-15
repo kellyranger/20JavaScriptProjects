06c50bf1072b6faa3ba652fd96758b40f4fdc91d
Kelly Ranger <kelly@losgatos-massage.com>
2020-07-15 12:55:35 -0700

    Fixed innerHTML error

--- hangman-kelly/script.js
+++ hangman-kelly/script.js
@@ -1,5 +1,5 @@
 const wordEl = document.getElementById('word');
-const wrongLettersEl = document.getElementById('wong-letters');
+const wrongLettersEl = document.getElementById('wrong-letters');
 const playAgainBtn = document.getElementById('play-again');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
