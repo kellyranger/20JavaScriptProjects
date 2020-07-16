5cf7e386b3e65bc53e3b61d68c149723d7997064
Kelly Ranger <kelly@losgatos-massage.com>
2020-07-15 18:03:03 -0700

    Complete Hangman project

--- hangman-kelly/script.js
+++ hangman-kelly/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -39,11 +39,31 @@ function displayWord(){
 //Update the Wrong Letters array
 function updateWrongLettersEl() {
   // console.log('Update wrong');
+
+  //display wrong letters
   wrongLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map(letter => `<span>${letter}</span>`)}
   `;
+
+  //display parts of hangman figure
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if(index < errors){
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+  //Check if game is lost
+  if(wrongLetters.length === figureParts.length){
+    finalMessage.innerText = 'Better luck next time.';
+    popup.style.display = 'flex';
+  }
 }
+
+
 
 //Show notification
 function showNotification(){
@@ -77,6 +97,21 @@ window.addEventListener('keydown', e => {
       }
     }
   }
+});
+
+//Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  //Empty arrays (correct and wrong letters)
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
 });
 
 displayWord();
