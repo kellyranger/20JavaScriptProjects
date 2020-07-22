a588f89b5cd1fed2ee71d5f392d321d1a1c220b2
Kelly Ranger <kelly@losgatos-massage.com>
2020-07-21 19:26:00 -0700

    Completed Music Player

---
+++ music-player-kelly/script.js
@@ -0,0 +1,112 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+//Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+//Keep track of song
+let songIndex = 2;
+
+//initially load the song details in to DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song){
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+//Play song
+function playSong(){
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play(); //from the audio api
+}
+
+//Pause song
+function pauseSong(){
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause(); //from the audio api
+}
+
+//Previous Song
+function prevSong() {
+  songIndex--;
+
+  if(songIndex < 0){
+    songIndex = songs.length -1;
+  }
+
+  loadSong([songs[songIndex]]);
+
+  playSong();
+}
+
+//Next Song
+function nextSong() {
+  songIndex++;
+
+  if(songIndex > songs.length - 1){
+    songIndex = 0;
+  }
+
+  loadSong([songs[songIndex]]);
+
+  playSong();
+}
+
+//Update progress bar
+function updateProgress(e){
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+//Set Progress bar
+function setProgress(e){
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+
+}
+
+//Event Listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if(isPlaying){
+    pauseSong();
+  }else {
+    playSong();
+  }
+});
+
+//Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time/song update event
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Song Ends
+audio.addEventListener('ended', nextSong);
