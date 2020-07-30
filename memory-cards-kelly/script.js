d7dd6e7c6cc68b8e858bf527dcbcec201e551cae
Kelly Ranger <kelly@losgatos-massage.com>
2020-07-30 09:50:16 -0700

    Completed Memory Cards

--- memory-cards-kelly/script.js
+++ memory-cards-kelly/script.js
@@ -17,21 +17,23 @@ let currentActiveCard = 0;
 //Store DOM cards
 const cardsEl = [];
 
+const cardsData = getCardsData();
+
 //Store Card Data (starting with hard coded object array then comment out)
-const cardsData = [
-  {
-    question: 'What must a variable begin with',
-    answer: 'A letter, $ or _'
-  },
-  {
-    question: 'What is a variable',
-    answer: 'A Container for a piece of data'
-  },
-  {
-    question: 'Example of Case Sensitive Variable',
-    answer: 'thisIsAVariable'
-  }
-];
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with',
+//     answer: 'A letter, $ or _'
+//   },
+//   {
+//     question: 'What is a variable',
+//     answer: 'A Container for a piece of data'
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable'
+//   }
+// ];
 
 //Create all cards
 function createCards(){
@@ -77,5 +79,86 @@ function updateCurrentText(){
   `;
 }
 
+//Get cards from local storage 
+//Note: to retrieve data from local storage 
+// - restore it to an array form with JSON.parse 
+//(to put in local storage it must be turned into a string
+//with JSON.stringify)
+function getCardsData(){
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [] : cards;
+}
+
+//Add card to local storage
+function setCardsData(cards){
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+
 
 createCards();
+
+//Event Listeners
+//Next Button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard +1;
+
+  if(currentActiveCard > cardsEl.length -1){
+    currentActiveCard = cardsEl.length -1;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+//Prevous Button
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if(currentActiveCard < 0){
+    currentActiveCard = 0;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+//Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+
+//Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+//Add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+  
+  if(question.trim() && answer.trim()){
+    const newCard = {question, answer};
+
+    //maybe add an alert if the conditions are not satisfied
+
+    createCard(newCard);
+
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+//Clear ALL cards button (note: add functionality to remove individual cards 
+//this function removes all cards in local storage)
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
+});
